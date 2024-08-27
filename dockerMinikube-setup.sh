b6269089ee1f0404187a17a79d25050dedeb9241
Daniel <dubuntu13@gmail.com>
2024-08-27 13:35:40 +0330

    add_setup-md

--- dockerMinikube-setup.sh
+++ dockerMinikube-setup.sh
@@ -29,47 +29,19 @@ fi
 echo "Updating packages..."
 sudo apt update & loading
 
-if [ $? -ne 0 ]; then
-    echo "Command failed"
-    exit 1
-else
-    echo "Now you are Updated!"
-fi
-
 # Now Install dependencies of Docker.
 echo "Installing dependencies..."
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y & loading
-
-if [ $? -ne 0 ]; then
-    echo "Command Failed!"
-    exit 1
-else
-    echo "Dependencies Installed!"
-fi
+sudo apt-get install ca-certificates curl gnupg lsb-release -y & loading
 
 # Now, add Docker’s GPG key and the Docker repository to your system.
 echo "Downloading Docker..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - & loading
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" & loading
 
-if [ $? -ne 0 ]; then
-    echo "Command Failed!"
-    exit 1
-else
-    echo "Docker downloaded!"
-fi
-
 # Now you can Install Docker.
 echo "Installing Docker..."
 sudo apt update & loading
 sudo apt install docker-ce -y & loading
-
-if [ $? -ne 0 ]; then
-    echo "Command Failed!"
-    exit 1
-else
-    echo "Docker Installed!"
-fi
 
 ##########
 
@@ -78,21 +50,12 @@ echo "Now we installing docker-compose"
 
 #Download docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-if [ $? -ne 0 ]; then
-    echo "Command FAILED!!!!!!!!"
-    exit 1
-else
-    echo "File Downloaded!"
-fi
-
 #change ownership
 sudo chmod +x /usr/local/bin/docker-compose
-if [ $? -ne 0 ]; then
-    echo "Command FAILED!!!!!!!!"
-    exit 1
-else
-    echo "Ownership has changed"
-fi
+
+#################Install minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
 
 #see Version of the docker-compose
 docker_version=$(docker-compose --version)
