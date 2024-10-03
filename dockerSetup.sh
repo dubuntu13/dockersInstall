2803ac86fb84fc51376ef2228b35ec707e6be8a0
Daniel <dubuntu13@gmail.com>
2024-10-03 19:48:12 +0330

    2.0

--- dockerSetup.sh
+++ dockerSetup.sh
@@ -52,16 +52,3 @@ echo "Now we installing docker-compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #change ownership
 sudo chmod +x /usr/local/bin/docker-compose
-
-#################Install minikube
-#curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-#sudo dpkg -i minikube_latest_amd64.deb
-#
-##see Version of the docker-compose
-#docker_version=$(docker-compose --version)
-#if [ $? -ne 0 ]; then
-#    echo "Command FAILED!!!!!!!!"
-#    exit 1
-#else
-#    echo "$docker_version"
-#fi
